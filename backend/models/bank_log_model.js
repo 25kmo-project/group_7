b262a413214c8ee8a6d9d3ff9e3866839e98fc38
Santeri <santerirautio17@gmail.com>
2026-01-10 18:51:08 +0200

    bank_log_models valmis

---
+++ backend/models/bank_log_model.js
@@ -0,0 +1,31 @@
+const db = require('../database');
+const bcrypt = require('bcryptjs');
+const { getAll, update } = require('./bank_user_model');
+
+const log = {
+    getAll: function (callback) {
+        return db.query("SELECT * FROM log", callback);
+    },
+    getOne: function (log_id, callback) {
+        return db.query("SELECT * FROM log WHERE log_id = ?",
+            [log_id], callback);
+    },
+    add: function (log, callback) {
+        return db.query("INSERT INTO log (actions, event_time, amount, account_id, user_id, card_id) VALUES (?,?,?,?,?,?)",
+            [log.actions, log.event_time, log.amount, log.account_id, log.user_id, log.card_id], callback);
+    },
+    update: function (log, callback) {
+        return db.query("UPDATE log SET actions = ?, event_time = ?, amount = ? WHERE log_id = ?",
+            [log.actions, log.event_time, log.amount, log_id], callback);
+    },
+    delete: function (log_id, callback) {
+        return db.query("DELETE * FROM log WHERE log_id = ?",
+            [log_id], callback);
+    }
+
+
+
+
+}
+
+module.exports = log;
