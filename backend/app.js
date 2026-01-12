5709229c5b4f310c3859d5c8e0c1bc7ed4902c19
Valtteri Sippala <sippava@users.noreply.github.com>
2026-01-12 11:32:00 +0200

    Kirjautuminen testattu ja toimii ja antaa tokenin.

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ var indexRouter = require('./routes/index');
 var bank_user_Router= require('./routes/bank_user');
 var account_Router= require('./routes/bank_account');
 var card_Router= require('./routes/bank_card');
+var kirjautuminen_router= require('./routes/bank_kirjautuminen');
 
 var app = express();
 
@@ -27,6 +28,8 @@ console.log("card_Router =", card_Router);
 app.use('/bank_user', bank_user_Router);
 app.use('/bank_account', account_Router);
 app.use('/bank_card', card_Router);
+app.use('/bank_kirjautuminen', kirjautuminen_router);   
+
 
 
 module.exports = app;
