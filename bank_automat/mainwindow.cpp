d2fa79c471d67cbf16bfc159925ad79e6144685b
Taapitoope <taapitoope@users.noreply.github.com>
2026-01-12 13:58:29 +0200

    login ikkunaan voi laittaa user+pin ja vastauksena saa token

--- bank_automat/mainwindow.cpp
+++ bank_automat/mainwindow.cpp
@@ -6,9 +6,32 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->btnLogin, &QPushButton::clicked, this, &MainWindow::btnLoginSlot);
+    manager=new QNetworkAccessManager(this);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::btnLoginSlot()
+{
+    QString url=Environment::base_url()+"bank_kirjautuminen";
+    QNetworkRequest request(url);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+    QJsonObject jObject;
+    jObject.insert("user_id", ui->textUsername->text());
+    jObject.insert("pin",ui->textPassword->text());
+    QJsonDocument jsonDoc(jObject);
+    reply=manager->post(request, jsonDoc.toJson());
+    connect(reply, &QNetworkReply::finished, this, &MainWindow::loginAction);
+
+
+}
+
+void MainWindow::loginAction()
+{
+    QByteArray responseData=reply->readAll();
+    qDebug()<<responseData;
 }
