542b9778e60b75e1922ac97d8d7c1bf86305e0b4
Taapitoope <taapitoope@users.noreply.github.com>
2026-01-14 15:50:45 +0200

    user_id tarkistus toimii

--- backend/models/bank_card_account_model.js
+++ backend/models/bank_card_account_model.js
@@ -9,28 +9,30 @@ const bankCardAccount = {
         return db.query('SELECT * FROM card_account WHERE card_id = ?', [card_id], callback);
     },
     add:function(card_account, callback) {
+        // Get card_user_id
         db.query('SELECT user_id FROM card WHERE card_id = ?', [card_account.card_id],
-            (err, results) => {
+            (err, card_result) => {
             if (err) {
-                return callback(err);
+                return callback(err, null);
             }
-            if (results.length === 0) 
-                return callback(new Error('korttia ei löydy'));
+            if (card_result.length === 0) 
+                return callback(new Error('korttia ei löydy'), null);
 
 
+                // Get account_user_id
                 db.query('SELECT user_id FROM account WHERE account_id = ?', [card_account.account_id],
-                    (err, results) => {
+                    (err, account_result) => {
                     if (err) {
-                        return callback(err);
-                    } if (results.length === 0)
-                        return callback(new Error('tiliä ei löydy'));
+                        return callback(err, null);
+                    } if (account_result.length === 0)
+                        return callback(new Error('tiliä ei löydy'), null);
 
-                    if (results[0].user_id !== results[0].user_id) {
+                    if (card_result[0].user_id !== account_result[0].user_id) {
                         return callback(new Error('kortin ja tilin käyttäjät eivät täsmää'));
                     }
 
-        return db.query('INSERT INTO card_account (card_id, account_id) VALUES (?, ?)',
-            [card_account.card_id, card_account.account_id], callback);
+                return db.query('INSERT INTO card_account (card_id, account_id) VALUES (?, ?)',
+                [card_account.card_id, card_account.account_id], callback);
     });
         });
     },
