1db5e3e8336f1de2342aaea4d3bfba307ba11741
aleksijohan <allu.jussila@hotmail.com>
2026-01-14 18:02:08 +0200

    card moduliin lisätty tarkitus, että pin pitää olla 4 numeroinen

--- backend/models/bank_card_model.js
+++ backend/models/bank_card_model.js
@@ -9,13 +9,28 @@ const card = {
         return db.query('SELECT * FROM card WHERE card_id = ?', [card_id], callback);
     },
     add:function(card,callback){
-        bcrypt.hash(card.card_pin_hash, 10, function(err, hash) {
-        return db.query('INSERT INTO card (card_number, card_type, expiration_date, user_id, card_pin_hash) VALUES (?, ?, ?, ?, ?)',
+        const pin = card.pin;
+        if (!/^\d{4}$/.test(pin)) {
+            return callback(new Error('PIN pitää olla 4-numeroinen'), null);
+        }
+
+        bcrypt.hash(pin, 10, function(err, hash) {
+            if (err) {
+                return callback(err, null);
+            }
+            return db.query('INSERT INTO card (card_number, card_type, expiration_date, user_id, card_pin_hash) VALUES (?, ?, ?, ?, ?)',
             [card.card_number, card.card_type, card.expiration_date, card.user_id, hash], callback);
-    });
+        });
     },
     update:function(card_id, card, callback){
+        const pin = card.card_pin_hash;
+        if (!/^\d{4}$/.test(pin)) {
+            return callback(new Error('PIN pitää olla 4-numeroinen'), null);
+        }
         bcrypt.hash(card.card_pin_hash, 10, function(err, hash) {
+            if (err) {
+                return callback(err, null);
+            }
             return db.query('UPDATE card SET card_number = ?, card_type = ?, expiration_date = ?, card_pin_hash = ? WHERE card_id = ?',
                 [card.card_number, card.card_type, card.expiration_date, hash, card_id], callback);
         });
