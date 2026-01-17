7760bb8ccb6b9eb0f4fd0fe1f74d2de2e2a6b4d5
Valtteri Sippala <sippava@users.noreply.github.com>
2026-01-17 09:14:55 +0200

    Credit_limit säädetty vakioksi 5000€

--- backend/models/bank_account_model.js
+++ backend/models/bank_account_model.js
@@ -10,6 +10,13 @@ const account = {
         return db.query('SELECT * FROM account WHERE account_id = ?', [account_id], callback);
     },
     add:function(account, callback) {
+
+        if (account.balance == null) { 
+        account.balance = 0; }
+
+        if (account.account_type === 'credit' && account.credit_limit == null) { //Asettaa credit_limitin oletusarvoksi 5000€
+            account.credit_limit = 5000;                                        //jos credit account eikä credit_limit määritetty
+        }
         return db.query('INSERT INTO account (account_type, account_number, balance, credit_limit, user_id) VALUES (?, ?, ?, ?, ?)',
             [account.account_type, account.account_number, account.balance, account.credit_limit, account.user_id], callback);
     },
