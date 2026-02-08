088c68e92aa219a03ec4b6200fc0fdbc6d168461
Santeri <santerirautio17@gmail.com>
2026-02-09 00:12:16 +0200

    Lisätty transaction luokkaan kommenointi doxygen dokumentaatiota varten ja muutettu nappien nimiä hieman loogisemmaksi.

--- bank_automat/transactions.h
+++ bank_automat/transactions.h
@@ -7,7 +7,15 @@
 namespace Ui {
 class Transactions;
 }
-
+/**
+ * @class Transactions
+ * @brief Näyttää käyttäjän tilitapahtumat.
+ *
+ * Tämä dialogi hakee ja näyttää tilitapahtumat taulukossa. Tapahtumien paginointi
+ * hoidetaan backendissä (10 tapahtumaa kerrallaan). JSON-muotoinen data
+ * näytetään kolmessa sarakkeessa: päivämäärä, tapahtuma ja summa.
+ * Sovellus muuntaa UTC-aikavyöhykkeellä olevat aikaleimat paikalliseen aikaan.
+ */
 class Transactions : public QDialog
 {
     Q_OBJECT
@@ -25,13 +33,13 @@ public:
     ~Transactions();
 
     /**
-     * @brief Asettaa backend-tokenin, jota käytetään talletuspyynnössä.
+     * @brief Asettaa backend-tokenin autentikointia varten.
      * @param t Token merkkijonona.
      */
     void setToken(const QString &t);
 
     /**
-     * @brief Asettaa tilin ID:n, jolle talletus tehdään.
+     * @brief Asettaa tilin ID:n, jonka tapahtumia tarkastellaan
      * @param id Tilin tunniste.
      */
     void setAccountId(int id);
@@ -43,28 +51,28 @@ public:
 
 private slots:
     /**
-     * @brief Palaa takaisin edelliseen näkymään.
+     * @brief Sulkee dialogin ja palaa takaisin edelliseen näkymään.
      */
     void btnCloseClicked();
 
     /**
-     * @brief Selataan tapahtumia taaksepäin.
+     * @brief Siirtyy tilitapahtumissa edelliselle sivulle (uudemmat tapahtumat).
      */
-    void btnPreviousClicked();
+    void btnNewerClicked();
 
     /**
-     * @brief Selataan tapahtumia eteenpäin.
+     * @brief Siirtyy tilitapahtumissa seuraavalle sivulle (vanhemmat tapahtumat).
      */
-    void btnNextClicked();
+    void btnOlderClicked();
 
 
 private:
-    Ui::Transactions *ui;   ///< Käyttöliittymäolio
+    Ui::Transactions *ui;       ///< Käyttöliittymäolio
 
-    QString token;       ///< Backend-token
-    int accountId = -1;  ///< Tili, jolle talletus tehdään
-    int currentOffset = 0;      ///< Määritetään kuinka monta riviä hypätään kun painetaan edellinen tai seuraava nappeja
-    const int pageSize = 10;    ///< Tapahtumien määrä per sivu
+    QString token;              ///< Autentikaatio tokeni
+    int accountId = -1;         ///< Tarkasteltavan tilin ID
+    int currentOffset = 0;      ///< Haun aloituskohta (offset) sivutusta varten
+    const int pageSize = 10;    ///< Sivukoko (tapahtumien määrä per sivu)
 };
 
 #endif // TRANSACTIONS_H
