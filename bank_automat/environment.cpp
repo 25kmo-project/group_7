5105ed194391b0414c0bd103b4ee9c1c2f634969
Santeri <santerirautio17@gmail.com>
2026-02-09 15:41:52 +0200

    Sertifikaatin haku onnistui joten nyt otetaan https yhteys käyttöön

--- bank_automat/environment.cpp
+++ bank_automat/environment.cpp
@@ -4,6 +4,7 @@ Environment::Environment() {}
 
 QString Environment::base_url()
 {
-    return "http://86.50.23.70/";
+    return "https://bankg7.tech/";
+    // return "http://86.50.23.70/";
     // return "http://127.0.0.1/";
 }
